5e6cd4f2b1aaf86b74d3865361535f0694fd46f0
69Nesta <petitromeo74@gmail.com>
2026-05-26 18:01:17 +0200

    refactor: remove unused imports and clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { CurrentSelectedElement } from '@/components/editor/overlay/current_selected_element';
 import { ActionTopLeft } from '@/components/action_top_left';
 import { ProjectModal } from '@/components/project_modal';
-import { useEffect, useRef, useState, type RefObject } from 'react';
+import { useRef, useState, type RefObject } from 'react';
 import { EditorCanvas } from '@/components/editor';
 import { ToolBar } from '@/components/editor/overlay/tool_bar';
-import { useNetworkStore } from './store/network_store';
-import { MapLoader } from './context/map_loader';
 import { Button } from '@/components/ui/button';
 import { ChevronLeft } from 'lucide-react';
 
@@ -16,33 +14,9 @@ import {
 } from '@/components/ui/resizable'
 import { RightPanel } from './components/right_panel/right_panel';
 
-
-const map: MapLoader = new MapLoader([
-	'# Easy Level 2: Simple fork with two paths',
-	'nb_drones: 4',
-	'',
-	'start_hub: start -0 0 [color=green]',
-	'hub: junction 1 0 [color=yellow max_drones=2]',
-	'hub: path_a 2 1 [color=blue]',
-	'hub: path_b 2 -1 [color=blue]',
-	'end_hub: goal 3 0 [color=red max_drones=3]',
-	'',
-	'connection: start-junction [max_link_capacity=2]',
-	'connection: junction-path_a',
-	'connection: junction-path_b',
-	'connection: path_a-goal',
-	'connection: path_b-goal'
-]);
-
-
 const App = () => {
-	const network = useNetworkStore();
 	const editorBoxRef: RefObject<HTMLDivElement | null> = useRef(null);
 	const [isRightPanelVisible, setIsRightPanelVisible] = useState(true);
-
-	useEffect(() => {
-		network.import(map.data);
-	}, []);
 
 	return (
 		<div className='flex w-screen h-screen'>
